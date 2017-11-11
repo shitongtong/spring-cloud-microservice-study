bd831921950b8039478c959d8039d64adea546b8
shitongtong <shitongtong@onlyedu.com>
2017-11-11 21:54:23 +0800

    create

---
+++ microservice-discovery-eureka/src/main/java/cn/stt/cloud/eureka/EurekaApplication.java
@@ -0,0 +1,11 @@
+package cn.stt.cloud.eureka;
+
+/**
+ * @Author shitongtong
+ * <p>
+ * Created by shitongtong on 2017/11/6.
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+}
