f0523337ea9ddc2108ac91b6d4556671278bd2e1
shitongtong <shitongtong@onlyedu.com>
2017-11-23 17:54:24 +0800

    add

---
+++ microservice-discovery-eureka/src/main/java/cn/stt/cloud/study/EurekaApplication.java
@@ -0,0 +1,20 @@
+package cn.stt.cloud.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 使用Eureka做服务发现
+ *
+ * @Author shitongtong
+ * <p>
+ * Created by shitongtong on 2017/11/6.
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
